e94c8a4fce188a664f8af46775d172f57dee4d65
Kimdahee <dh980502@gmail.com>
2024-07-12 10:51:36 +0900

    feat : icon box component 작성

---
+++ lib/presentation/component/icon_box.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class IconBox extends StatelessWidget {
+  final String iconImage;
+
+  const IconBox({super.key, required this.iconImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF1F9FA),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(13.0),
+          child: Image.asset(
+            iconImage,
+            width: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
