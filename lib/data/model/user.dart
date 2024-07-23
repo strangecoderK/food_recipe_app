e0534e6c421218f7113f8fa781fd73c7e87dc63e
Kimdahee <dh980502@gmail.com>
2024-07-23 15:00:51 +0900

    feat : user model 작성

---
+++ lib/data/model/user.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required int id,
+    required String name,
+    required String email,
+    required String image,
+  }) = _User;
+
+  factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+}
